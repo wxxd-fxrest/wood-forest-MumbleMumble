7f8f09bd5bcf7ddd2683449d4a8c5454901bf194
wxxd-fxrest <fxrest704@gmail.com>
2023-04-05 01:38:45 +0900

    감정이모티콘 선택, 출력 / 카드 별도 선택 기능 구현 및 출력 / 공개여부에 따라 출력 값 변경

--- src/routers/Post/Post.js
+++ src/routers/Post/Post.js
@@ -1,3 +1,5 @@
+import None  from '../../Image/Mumble_Profile_None.PNG' ; 
+
 const Post = ({postData}) => {
 
     return (
@@ -6,13 +8,16 @@ const Post = ({postData}) => {
             <div style={{backgroundImage: `url(${postData.Data.cardImgUrl})`, height:"300px"}}>
                 {postData.Data.anonymous == true && 
                     <h5 style={{backgroundColor:"red"}}> {postData.Data.displayName} </h5>}
+                {postData.Data.anonymous == true ?
+                    <img src={postData.Data.attachmentUrl} width="80px"/>: 
+                    <img src={None} width="80px"/>}
                 <h3> {postData.Data.PostText} </h3>
                 <h4> {postData.Data.Music} </h4>
                 <h4> {postData.Data.artist} </h4>
                 {postData.Data.selected == "좋아" && <p>🤗</p>}
                 {postData.Data.selected == "화나" && <p>🤬</p>}
                 {postData.Data.selected == "슬퍼" && <p>😢</p>}
-            </div> : <div style={{backgroundColor:"yellow"}}>
+            </div> : <div style={{backgroundColor:"skyblue"}}>
                 {postData.Data.anonymous == true && 
                     <h5 style={{backgroundColor:"red"}}> {postData.Data.displayName} </h5>}
                 <h3> {postData.Data.PostText} </h3>
--- src/routers/Profile/Profile.js
+++ src/routers/Profile/Profile.js
@@ -1,18 +1,23 @@
+import None  from '../../Image/Mumble_Profile_None.PNG' ; 
+
 const Profile = ({currentPost}) => {
-    console.log(currentPost)
+    // console.log(currentPost)
     return(
         <div>
         {currentPost.Data.cardImgUrl ? 
             <div style={{backgroundImage: `url(${currentPost.Data.cardImgUrl})`, height:"300px"}}>
                 {currentPost.Data.anonymous == true && 
                     <h5 style={{backgroundColor:"red"}}> {currentPost.Data.displayName} </h5>}
+                {currentPost.Data.anonymous == true ?
+                    <img src={currentPost.Data.attachmentUrl} width="80px"/>: 
+                    <img src={None} width="80px"/>}
                 <h3> {currentPost.Data.PostText} </h3>
                 <h4> {currentPost.Data.Music} </h4>
                 <h4> {currentPost.Data.artist} </h4>
                 {currentPost.Data.selected == "좋아" && <p>🤗</p>}
                 {currentPost.Data.selected == "화나" && <p>🤬</p>}
                 {currentPost.Data.selected == "슬퍼" && <p>😢</p>}
-            </div> : <div style={{backgroundColor:"skyblue"}}>
+            </div> : <div style={{backgroundColor:"yellow"}}>
                 {currentPost.Data.anonymous == true && 
                     <h5 style={{backgroundColor:"red"}}> {currentPost.Data.displayName} </h5>}
                 <h3> {currentPost.Data.PostText} </h3>
